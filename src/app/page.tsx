46eee6861d71dbabbb8469bf44dedfd2f9ab3e08
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-09-13 14:42:12 -0300

    feat: theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeDropdown } from "@/components/themeDropDown";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <Button className="bg-red-400">teste</Button>
+      <Button>teste</Button>
+      <ThemeDropdown />
     </div>
   );
 }
